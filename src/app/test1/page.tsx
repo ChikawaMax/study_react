a4a822bed29f580493d91303af83a379ad25066d
本間敬太 <keitakunv2@gmail.com>
2024-06-29 13:25:38 +0900

    イベント

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 const HomePage = () => {
 
-  const handleClick = () => {
-    console.log("aaaaa")
+  const handleClick = (e: any) => {
+    console.log(e.target.textContent);
   }
 
   return (
     <div>
       <h1>aaa</h1>
-      <input type='button' value="aaaaa" onClick={handleClick} />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
